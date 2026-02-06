c06ba1b5fb5468974c40e00dc55c380f59976d8e
fasiha.codes <fasiha.codes@gmail.com>
2026-02-06 20:37:29 +0500

    Add project files.

---
+++ Practice task.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+//Task1
+/*int main()
+{
+	printf("Step 1\n\tStep 2\n\t\tStep 3");
+	return 0;
+}*/
+//Task2
+/*int main()
+{
+	int a=1, b=123, c=12345;
+	printf("%5d\n", a);
+	printf("%6d\n", b);
+	printf("%7d",c);
+	return 0;
+}*/
+//Task3
+/*int main()
+{
+	double pi = 3.14159;
+	printf("%.1lf\n%.3lf\n%.5lf\n",pi,pi,pi);
+
+	return 0;
+}*/
+//Task4
+// (carrige return \r moves the cursor to
+// the beginning of current line without 
+// moving to the next line)
+/*int main()
+{
+	printf("Loading");
+	getchar();
+	printf("\rComplete!");
+	return 0;
+}*/
+//Task5
+// (*modifier which allows printf 
+// to take field width from an argument
+// instead of the format string)
+/*int main()
+{
+	printf("%*c\n",5,'X');
+	printf("%*c\n", 10, 'X');
+	printf("%*c\n", 15, 'X');
+	return 0;
+}*/
---
+++ Self practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+/* int main(){
+	//program 1
+	int a, b, c, d, result;
+	printf("Enter a");
+	scanf_s("%d", &a);
+	printf("Enter b");
+	scanf_s("%d", &b);
+	printf("Enter c");
+	scanf_s("%d", &c);
+	printf("Enter d");
+	scanf_s("%d", &d);
+	printf("Result=%d", result = a * b);
+	return 0;
+}*/
